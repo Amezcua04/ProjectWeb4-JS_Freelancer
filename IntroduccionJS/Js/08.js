e6d463830f20c5a523eb5421e55856934abf6937
CarlosAmezcua <Amezcua041196@gmail.com>
2022-08-19 02:24:24 -0600

    update
    Tipos de datos en JavaScript. concatenación de los String o cadenas de texto, usando la concatenación sencilla y los templates con variables.

---
+++ IntroduccionJS/Js/08.js
@@ -0,0 +1,8 @@
+const nombre = 'Carlos';
+const email = 'correo@correo.com';
+
+//concatenación
+console.log("Nombre Cliente: " + nombre +" Email: "+ email);
+
+//template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
