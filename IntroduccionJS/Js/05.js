d1a6d13fe1ca5962d8f5ceebf66eb83a729b9f1e
CarlosAmezcua <Amezcua041196@gmail.com>
2022-08-19 01:30:02 -0600

    update numeros
    Tipos de datos en JavaScript.
    numeros y algunas operaciones.

---
+++ IntroduccionJS/Js/05.js
@@ -0,0 +1,16 @@
+// Tipos de Datos en JavaScript
+
+// Números
+// No existen diferentes tipos de datos (entero, doble, flotante, decimal, etc).
+// Todos pertenecen al mismo tipo de dato que es el numero.
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 10;
+const numero4 = 6;
+const numero5 = -5;
+
+console.log(numero1 + numero2); //suma
+console.log(numero2 - numero1); //resta
+console.log(numero2 * numero1); //multiplicación
+console.log(numero2 / numero1); //división
+console.log(numero3 % numero4); //residuo
