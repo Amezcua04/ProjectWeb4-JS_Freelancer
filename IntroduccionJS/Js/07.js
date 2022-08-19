d9cd41d2535124792b12fa9cfaf3daf8d14496af
CarlosAmezcua <Amezcua041196@gmail.com>
2022-08-19 02:12:01 -0600

    update operadores
    Tipos de datos en JavaScript.
    orden de las operaciones e incrementos/decrementos en 1 o n.

---
+++ IntroduccionJS/Js/07.js
@@ -0,0 +1,26 @@
+// Orden de las operaciones
+
+//funciones exclusivas de los números
+// una operación prioriza lo que hay dentro de un parentesís, después, la multiplicación y división, etc.
+// es el mismo orden operacional que en mátematicas
+let resultado;
+resultado = (20 + 30) * 2;
+resultado = (100 + 200 + 300) * .2;
+
+// Incrementos y Decrementos
+let puntaje = 10;
+ // incremento en 1
+ puntaje++;
+ ++puntaje;
+
+ // decremento en 1
+ puntaje--;
+ --puntaje;
+
+ //incremento en n
+ puntaje += 10;
+
+ //Decremento en n
+ puntaje -= 10;
+
+console.log(puntaje);
