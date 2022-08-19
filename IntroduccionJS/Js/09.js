12f311230ff026c55f622d8a2ba9a0f691fdd087
CarlosAmezcua <Amezcua041196@gmail.com>
2022-08-19 02:30:37 -0600

    update booleans
    Tipos de datos en JavaScript.
    Booleanos o flags (true or false).

---
+++ IntroduccionJS/Js/09.js
@@ -0,0 +1,12 @@
+// Valores Booleanos (verdadero / falso)
+// tambien conocidos como flags o banderas
+
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1);
+console.log(boolean2);
+
+const boolean3 = new Boolean(true);
+console.log(boolean3);
+
