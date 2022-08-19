2837c2234dbc10e8e69eb1b853a9a84487537cd3
CarlosAmezcua <Amezcua041196@gmail.com>
2022-08-19 01:51:15 -0600

    update numeros
    Tipos de datos en JavaScript.
    Object Math métodos matematicos entre números.

---
+++ IntroduccionJS/Js/06.js
@@ -0,0 +1,18 @@
+// Objeto Math
+
+// funciones mátematicas
+//let resultado = Math.PI;
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.6); //redondear hacia el entero mas cercano
+resultado = Math.ceil(2.2); // siempre redondear hacia arriba
+resultado = Math.floor(2.9); //siempre redondear hacia abajo
+resultado = Math.sqrt(144); //raiz cuadrada
+resultado = Math.abs(-200); //valor absoluto
+resultado = Math.min(3, 5, 1, 8, 2, 10); //valor mínimo de un listado
+resultado = Math.max(3, 5, 1, 8, 2, 10); //valor máximo de un listado
+resultado = Math.random(); //número aleatorio
+resultado = Math.floor(Math.random()*10); //número aleatorio entero
+
+console.log(resultado);
